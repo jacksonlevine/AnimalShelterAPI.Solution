649df062ca8e6168546de295afaa50bcd1e59ab3
jacksonlevine <jack@netbug.us>
2023-03-31 11:55:26 -0700

    Remove placeholder objects

--- Models/AnimalShelterApiContext.cs
+++ Models/AnimalShelterApiContext.cs
@@ -9,16 +9,5 @@ namespace AnimalShelterApi.Models
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
     {
     }
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-      builder.Entity<Animal>()
-        .HasData(
-          new Animal { AnimalId = 1, Name = "Matilda", Species = "Cat", Age = 7 },
-          new Animal { AnimalId = 2, Name = "Rexie", Species = "Dog", Age = 10 },
-          new Animal { AnimalId = 3, Name = "Matilda", Species = "Dog", Age = 2 },
-          new Animal { AnimalId = 4, Name = "Pip", Species = "Owl", Age = 4 },
-          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Parakeet", Age = 22 }
-        );
-    }
   }
 }
